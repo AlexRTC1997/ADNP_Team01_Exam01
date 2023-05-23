09db8fd5eb4ff37d1afd51a0673a50abe80304ae
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-05-23 08:13:05 -0500

    ADD: Project created

---
+++ app/src/main/java/com/example/danp_team01_exam01/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.danp_team01_exam01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.danp_team01_exam01.ui.theme.DANP_Team01_Exam01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DANP_Team01_Exam01Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DANP_Team01_Exam01Theme {
+        Greeting("Android")
+    }
+}
